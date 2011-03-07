545201559b7ee895b34ea2ace433b28d88f98f74
Nugroho Herucahyono <xinuc@xinuc.org>
2011-03-07 11:44:23 +0700

    regenerate java file form JSON class

--- org/xinuc/jason/JSON.java
+++ org/xinuc/jason/JSON.java
@@ -1,6 +1,11 @@
 // Generated from JSON.mirah
 package org.xinuc.jason;
 public class JSON extends java.lang.Object {
+  public static java.lang.Object parse(java.io.InputStream stream) {
+    org.xinuc.jason.JSONUtils jsonUtils = null;
+    jsonUtils = new org.xinuc.jason.JSONUtils();
+    return this.parse(jsonUtils.streamToString(stream));
+  }
   public static java.lang.Object parse(java.lang.String str) throws org.xinuc.jason.JSONException {
     org.xinuc.jason.JSONTokenizer token = null;
     char c = 0;
